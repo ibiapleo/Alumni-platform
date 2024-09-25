a904ea525c213ab01edcc287f7026d7836ae24dd
ibiapleo <leonardojose159357@gmail.com>
2024-09-25 19:23:31 -0300

    Removendo a pasta registro, já que foi renomeada

--- registro/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('registrar/', views.registrar_usuario, name='registrar_usuario'),
-    path('enviar-codigo/', views.send_verification_code, name='enviar_codigo'),
-    path('confirmar-codigo/', views.confirm_code, name='confirmar_codigo'),
-    path('reenviar-codigo/', views.send_verification_code, name='reenviar_codigo'),
-]
