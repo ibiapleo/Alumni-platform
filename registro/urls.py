3bd6dfe69ecdfaaaab0d545edb866dae32cb49de
ibiapleo <leonardojose159357@gmail.com>
2024-09-25 18:13:51 -0300

    Adicionando verificação de código por email no cadastro

--- registro/urls.py
+++ registro/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('registrar/', views.registrar_usuario, name='registrar_usuario'),
+    path('enviar-codigo/', views.send_verification_code, name='enviar_codigo'),
+    path('confirmar-codigo/', views.confirm_code, name='confirmar_codigo'),
+    path('reenviar-codigo/', views.send_verification_code, name='reenviar_codigo'),
 ]
--- registro/views.py
+++ registro/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from .forms import RegistroForm
+from django.core.mail import send_mail
+from django.utils.crypto import get_random_string
+from django.contrib import messages
+from django.http import JsonResponse
+import json
 
-def registrar_usuario(request):
+def register_user(request):
     if request.method == 'POST':
         form = RegistroForm(request.POST)
         if form.is_valid():
@@ -12,3 +17,36 @@ def registrar_usuario(request):
     else:
         form = RegistroForm()
     return render(request, 'registro/registro.html', {'form': form})
+
+def send_verification_code(request, user_email):
+    code = get_random_string(length=6, allowed_chars='0123456789')
+    
+    request.session['verification_code'] = code
+    
+    send_mail(
+        'Seu código de verificação',
+        f'Por favor, use o seguinte código para verificar sua conta: {code}',
+        'from@example.com',
+        [user_email],
+        fail_silently=False,
+    )
+    
+    return redirect('verificacao_de_codigo')
+
+def confirm_code(request):
+    if request.method == 'POST':
+        code_entered = ''.join(request.POST.getlist('code'))
+        code_sent = request.session.get('verification_code')
+        data = json.loads(request.body)
+        
+        if code_entered == code_sent:
+            messages.success(request, 'E-mail verificado com sucesso!')
+            return JsonResponse({'success': True})
+        else:
+            messages.error(request, 'Código inválido. Tente novamente.')
+            return JsonResponse({'success': False})
+      
+    
+    return render(request, 'registro/verificacao_de_codigo.html')
+
+
